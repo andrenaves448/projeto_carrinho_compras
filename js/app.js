ee72ed48cb29115ad72c80063f67847ac7013fba
André <andrenaves448@gmail.com>
2025-08-17 19:07:49 -0300

    Refatore a lógica de adição de produtos para remover alertas e garantir o correto parseamento dos valores

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@ function adicionar() {
     //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-
-
-
+    let valorUnitario = parseFloat(produto.split('R$')[1]);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
     //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML =  carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
     //adicionar o produto no carrinho
     //atualizar o subtotal
 
