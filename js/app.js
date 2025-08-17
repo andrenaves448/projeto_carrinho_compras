d46b61b9e73f9e518b404602e206d5dc3ac5824c
André <andrenaves448@gmail.com>
2025-08-17 18:30:39 -0300

    Implement product addition functionality in shopping cart

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+
+    //calcular o preço, o nosso subtotal
+    //adicionar o produto no carrinho
+    //atualizar o subtotal
+
+}
+function limpar(){
+
+}
+    
